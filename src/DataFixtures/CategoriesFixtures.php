b320b4bc60f60bf244e7c43b3b036ca27d0324ec
CAMARA <88945535+camara92@users.noreply.github.com>
2022-12-11 03:02:58 +0100

    fixture avec utilisation de hash password et object manager interface

--- src/DataFixtures/CategoriesFixtures.php
+++ src/DataFixtures/CategoriesFixtures.php
@@ -14,18 +14,50 @@ class CategoriesFixtures extends Fixture
     }
     public function load(ObjectManager $manager): void
     {
-        $parent = new Categories();
-        $parent->setName('Informatique 4.0');
-        $parent->setSlug($this->slugger->slug(($parent->getName()))->lower());
+        // $parent = new Categories();
+        // $parent->setName('Informatique 4.0');
+        $parent = $this->createCategory('Informatique' ,null, $manager );
+        // $parent = $this->createCategory(name : 'Informatique' ,manager: $manager );
+        // $parent->setSlug($this->slugger->slug(($parent->getName()))->lower());
         
-        $manager->persist($parent);
-        $category = new Categories();
-        $category->setName('Ordinateur portables');
-        $category->setSlug($this->slugger->slug(($category->getName()))->lower());
-        $category->setParent($parent);
+        // $manager->persist($parent);
+
+        // deuxième categories 
+
+        // $category = new Categories();
+        // $category->setName('Ordinateur portables');
+
+        $category = $this->createCategory('Ordinateur Portable', $parent, $manager);
+        $category = $this->createCategory('Ecrans', $parent, $manager);
+        $category = $this->createCategory('Souris', $parent, $manager);
+        $category = $this->createCategory('Chargeurs', $parent, $manager);
+        $category = $this->createCategory('PC fixes', $parent, $manager);
+      
+        // $category->setSlug($this->slugger->slug(($category->getName()))->lower());
+        // $category->setParent($parent);
+
+        // Modes 
+        $parent = $this->createCategory('Modes' ,null, $manager );
+        
+        $category = $this->createCategory('Hommes', $parent, $manager);
+        $category = $this->createCategory('Femmes', $parent, $manager);
+        $category = $this->createCategory('Enfants', $parent, $manager);
+       
 
         $manager->persist($category);
 
         $manager->flush();
+    }
+    public function createCategory(string $name, Categories $parent = null , ObjectManager $manager){
+
+        $category = new Categories();
+        // $category->setName('Ordinateur portables');
+        $category->setName($name);
+        $category->setSlug($this->slugger->slug(($category->getName()))->lower());
+        $category->setParent($parent);
+        $manager->persist($category);
+
+        return $category;
+
     }
 }
--- src/DataFixtures/UsersFixtures.php
+++ src/DataFixtures/UsersFixtures.php
@@ -3,41 +3,37 @@
 namespace App\DataFixtures;
 
 use App\Entity\Users; 
-use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class UsersFixtures extends Fixture
 {
+    public function __construct(private  UserPasswordHasherInterface $passwordEncoder, private SluggerInterface $lugger )
+    {
+
+    }
     public function load(ObjectManager $manager): void
     {
-        $user = new Users();
+        $admin = new Users();
        
-            $user->setemail('daoudasouleymanecamara8@gmail.com')
+            $admin->setemail('daoudasouleymanecamara8@gmail.com')
             ->setFirstname('Daouda')
             ->setLastname('CAMARA')
-            ->setPassword('daouda326832')
+            // hash du password 
+            ->setPassword(
+                $this->passwordEncoder->hashPassword($admin, 'daouda326832')
+            )
             ->setAdress('76 Rue du Repos')
             ->setZipcode(69007)
             ->setCity('Lyon')
+            ->setRoles(['ROLE_ADMIN'])
             
             ;
        
-        $manager->persist($user);
-        // 2
-        $user = new Users();
-       
-            $user->setemail('daoudacamara8@gmail.com')
-            ->setFirstname('Daoud')
-            ->setLastname('CAMAR')
-            ->setPassword('daouda32632')
-            ->setAdress('76 Rue du Rep')
-            ->setZipcode(69107)
-            ->setCity('Lyon')
-            
-            ;
-       
-        $manager->persist($user);
-
+        $manager->persist($admin);
+    
         $manager->flush();
     }
 }
