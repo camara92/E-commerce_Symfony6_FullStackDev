9758ca5220b572aecb2b40fe13b56ea8ee3de442
CAMARA <88945535+camara92@users.noreply.github.com>
2022-12-11 02:31:42 +0100

    sluggerinterface lower

--- src/DataFixtures/CategoriesFixtures.php
+++ src/DataFixtures/CategoriesFixtures.php
@@ -2,21 +2,26 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Persistence\ObjectManager;
 use App\Entity\Categories; 
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
 class CategoriesFixtures extends Fixture
 {
+    public function __construct(private SluggerInterface $slugger){
+        // 
+    }
     public function load(ObjectManager $manager): void
     {
         $parent = new Categories();
-        $parent->setName('Informatique');
-        $parent->setSlug('informatique');
+        $parent->setName('Informatique 4.0');
+        $parent->setSlug($this->slugger->slug(($parent->getName()))->lower());
         
         $manager->persist($parent);
         $category = new Categories();
-        $category->setName('Ordinateur');
-        $category->setSlug('pcpcpce');
+        $category->setName('Ordinateur portables');
+        $category->setSlug($this->slugger->slug(($category->getName()))->lower());
         $category->setParent($parent);
 
         $manager->persist($category);
